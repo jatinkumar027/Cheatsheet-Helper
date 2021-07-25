fc40dbde9d56cfe92ed2fdeb6e326a8c40b47164
Jatin Kumar <jatink220@gmail.com>
2021-07-25 14:30:17 +0530

    Bug fixed

--- public/js/script.js
+++ public/js/script.js
@@ -9,7 +9,7 @@ const content = document.getElementsByClassName('content');
 searchInput.addEventListener('keyup',()=>{
     let inputValue = searchInput.value;
     for (i = 0; i < h3Tags.length; i++) {
-        if (!h3Tags[i].innerHTML.toLowerCase().includes(inputValue)) {
+        if (!h3Tags[i].innerHTML.toLowerCase().includes(inputValue.toLowerCase())) {
             codeSnippet[i].style.display="none";
 
         }
